d57c60cd33128888edca930c6e0190e30abd58f2
jxrvll <jxrvll@gmail.com>
2020-10-29 16:53:22 -0400

    Updates

---
+++ precache-manifest.29fc6971ec97d581326a6c366fb67253.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec24b964a500f29015dd4e80009e455",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "099abff33456db8c4b36",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "aadd6e599838c56c0285",
+    "url": "/react-portfolio/static/css/main.c72602b8.chunk.css"
+  },
+  {
+    "revision": "099abff33456db8c4b36",
+    "url": "/react-portfolio/static/js/2.5f5a78b7.chunk.js"
+  },
+  {
+    "revision": "48708d0bba57d355d4ffc14a8bfac5cf",
+    "url": "/react-portfolio/static/js/2.5f5a78b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aadd6e599838c56c0285",
+    "url": "/react-portfolio/static/js/main.2f329faa.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "9c61161c4d0474182b65e2642fb9a3dd",
+    "url": "/react-portfolio/static/media/asi.9c61161c.PNG"
+  },
+  {
+    "revision": "57c3a1a49970904a3bd3eddebb23f47a",
+    "url": "/react-portfolio/static/media/broc.57c3a1a4.PNG"
+  },
+  {
+    "revision": "84b1ad0ebb98657fe5f9f1bc4e651849",
+    "url": "/react-portfolio/static/media/calyx.84b1ad0e.PNG"
+  },
+  {
+    "revision": "b0b3788cf709369d16216b5868d04e42",
+    "url": "/react-portfolio/static/media/covid-dash.b0b3788c.PNG"
+  },
+  {
+    "revision": "cbc6bc46a735627adb2ac9073e52450c",
+    "url": "/react-portfolio/static/media/employee-dir.cbc6bc46.PNG"
+  },
+  {
+    "revision": "3758490d45b773b31fe1c8ca8a7808c0",
+    "url": "/react-portfolio/static/media/game-swap.3758490d.PNG"
+  },
+  {
+    "revision": "940bc44fff16ea1dd3108d78624f1a09",
+    "url": "/react-portfolio/static/media/gcc.940bc44f.PNG"
+  },
+  {
+    "revision": "52bac956e08977f33f31516f0a698f82",
+    "url": "/react-portfolio/static/media/google-books.52bac956.PNG"
+  },
+  {
+    "revision": "5423d188c9a316439dd9d3ad6322ff36",
+    "url": "/react-portfolio/static/media/me.5423d188.jpg"
+  },
+  {
+    "revision": "5f889e23db7af204567519fd713d2272",
+    "url": "/react-portfolio/static/media/wavy-motion-top.5f889e23.png"
+  },
+  {
+    "revision": "7af15d09175466037369bf4d5f1c3da2",
+    "url": "/react-portfolio/static/media/weather.7af15d09.PNG"
+  },
+  {
+    "revision": "cb0ff4c24c6be73dc5d253410e63d05c",
+    "url": "/react-portfolio/static/media/winehub.cb0ff4c2.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be9f749bba758350fc0200c7c0dd5b10.js"
+  "/react-portfolio/precache-manifest.29fc6971ec97d581326a6c366fb67253.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
